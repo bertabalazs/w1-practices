790b6276a8a67c76d21062def65867baa63b83e7
Berta Balazs <berta.balazs@gmail.com>
2022-01-19 16:48:33 +0100

    Getting familier with html and js

---
+++ frontend/script.js
@@ -0,0 +1,14 @@
+function loadEvent() {
+    console.log("Az oldal betöltődött");
+
+    const list = document.querySelectorAll("section");
+
+    for (const elem of list) {
+        elem.insertAdjacentHTML("beforeend", `
+        <p>text</p>
+        `);
+        //elem.querySelector("h1").outerHTML = ""
+    }
+}
+window.addEventListener("load", loadEvent);
+
