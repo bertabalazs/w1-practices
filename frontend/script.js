be5611a17ae4963dbbf1ffd975dc9bed701f8f24
Berta Balazs <berta.balazs@gmail.com>
2022-01-20 15:11:36 +0100

    adatok kiválasztása

--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,23 @@
-function loadEvent() {
+
+
+async function loadEvent() {
     console.log("Az oldal betöltődött");
 
-    const list = document.querySelectorAll("section");
+    const root = document.querySelector("#root")
 
-    for (const elem of list) {
-        elem.insertAdjacentHTML("beforeend", `
-        <p>text</p>
-        `);
-        //elem.querySelector("h1").outerHTML = ""
+    const countryRes = await fetch("https://restcountries.com/v3.1/all");
+    const countryArr = await countryRes.json();
+
+    //console.log(countryArr[0].name.common)
+
+    for (const country of countryArr) {
+        root.insertAdjacentHTML("beforeend", `
+            <section>
+                <h1>${country.name.common}</h1>
+                <p>${country.region}</p>
+            </section>
+        `)
+        //console.log(country.name.common);
     }
 }
 window.addEventListener("load", loadEvent);
-
